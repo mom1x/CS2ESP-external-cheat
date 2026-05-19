ba5230e750a1909382984f43112bfe8d237f581b
Mom1x <momixpro8@gmail.com>
2026-05-19 20:10:35 +0700

    Update CS2ESP.py

--- CS2ESP.py
+++ CS2ESP.py
@@ -26,7 +26,6 @@ m_hPlayerPawn = client_dll['client.dll']['classes']['CCSPlayerController']['fiel
 m_iHealth = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_iHealth']
 
 # Ожидаем запуска cs2.exe
-print("Ожидание запуска cs2.exe...")
 while True:
     time.sleep(1)
     try:
@@ -38,7 +37,9 @@ while True:
 
 time.sleep(1)
 os.system("cls")
-print("CS2 обнаружена. Запуск оверлея...")
+
+pm = pymem.Pymem("cs2.exe")
+client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
 def w2s(mtx, posx, posy, posz, width, height):
     screenW = mtx[12]*posx + mtx[13]*posy + mtx[14]*posz + mtx[15]
@@ -53,104 +54,91 @@ def w2s(mtx, posx, posy, posz, width, height):
     return [-999, -999]
 
 def esp(draw_list):
+    # Считываем view matrix
+    view_matrix = [pm.read_float(client + dwViewMatrix + i * 4) for i in range(16)]
+
+    # Получаем локального игрока и его команду
+    local_player = pm.read_longlong(client + dwLocalPlayerPawn)
     try:
-        # Считываем view matrix
-        view_matrix = [pm.read_float(client + dwViewMatrix + i * 4) for i in range(16)]
-
-        # Получаем локального игрока и его команду
-        local_player = pm.read_longlong(client + dwLocalPlayerPawn)
-        if local_player <= 0 or local_player > 0x7FFFFFFFFFFFFFFF:
-            return
-            
         local_team = pm.read_int(local_player + m_iTeamNum)
-
-        # Перебираем возможные слоты сущностей
-        for i in range(64):
-            entity = pm.read_longlong(client + dwEntityList)
-            if entity <= 0 or entity > 0x7FFFFFFFFFFFFFFF:
-                continue
-
-            # Находим адрес entity_controller
-            addr1 = entity + ((8 * (i & 0x7FFF) >> 9) + 16)
-            if addr1 <= 0 or addr1 > 0x7FFFFFFFFFFFFFFF: continue
-            list_entry = pm.read_longlong(addr1)
-            if not list_entry:
-                continue
-
-            addr2 = list_entry + (120) * (i & 0x1FF)
-            if addr2 <= 0 or addr2 > 0x7FFFFFFFFFFFFFFF: continue
-            entity_controller = pm.read_longlong(addr2)
-            if not entity_controller:
-                continue
-
-            # Получаем pawn из контроллера
-            entity_controller_pawn = pm.read_longlong(entity_controller + m_hPlayerPawn)
-            if not entity_controller_pawn:
-                continue
-
-            # Достаем сам pawn
-            addr3 = entity + (0x8 * ((entity_controller_pawn & 0x7FFF) >> 9) + 16)
-            if addr3 <= 0 or addr3 > 0x7FFFFFFFFFFFFFFF: continue
-            list_entry = pm.read_longlong(addr3)
-            if not list_entry:
-                continue
-
-            addr4 = list_entry + (120) * (entity_controller_pawn & 0x1FF)
-            if addr4 <= 0 or addr4 > 0x7FFFFFFFFFFFFFFF: continue
-            entity_pawn = pm.read_longlong(addr4)
-            if not entity_pawn or entity_pawn == local_player:
-                continue
-
-            # Проверяем жив ли (lifeState == 256)
-            if pm.read_int(entity_pawn + m_lifeState) != 256:
-                continue
-
-            # Пропускаем тиммейтов
-            if pm.read_int(entity_pawn + m_iTeamNum) == local_team:
-                continue
-
-            # Цвет бокса
-            color = imgui.get_color_u32_rgba(1, 0, 0, 1)
-
-            # Достаем указатель на матрицу костей
-            game_scene = pm.read_longlong(entity_pawn + m_pGameSceneNode)
-            if game_scene <= 0 or game_scene > 0x7FFFFFFFFFFFFFFF: continue
-            
-            bone_matrix = pm.read_longlong(game_scene + m_modelState + 0x80)
-            if bone_matrix <= 0 or bone_matrix > 0x7FFFFFFFFFFFFFFF: continue
-
-            try:
-                # Координаты головы
-                headX = pm.read_float(bone_matrix + 6 * 0x20)
-                headY = pm.read_float(bone_matrix + 6 * 0x20 + 0x4)
-                headZ = pm.read_float(bone_matrix + 6 * 0x20 + 0x8) + 8
-                head_pos = w2s(view_matrix, headX, headY, headZ, WINDOW_WIDTH, WINDOW_HEIGHT)
-
-                # Координаты ног
-                legZ = pm.read_float(bone_matrix + 28 * 0x20 + 0x8)
-                leg_pos = w2s(view_matrix, headX, headY, legZ, WINDOW_WIDTH, WINDOW_HEIGHT)
-
-                # Рисуем бокс
-                delta = abs(head_pos[1] - leg_pos[1])
-                leftX = head_pos[0] - delta // 3
-                rightX = head_pos[0] + delta // 3
-
-                # Линии бокса
-                draw_list.add_line(leftX, leg_pos[1], rightX, leg_pos[1], color, 2.0)
-                draw_list.add_line(leftX, leg_pos[1], leftX, head_pos[1], color, 2.0)
-                draw_list.add_line(rightX, leg_pos[1], rightX, head_pos[1], color, 2.0)
-                draw_list.add_line(leftX, head_pos[1], rightX, head_pos[1], color, 2.0)
-
-                # Читаем HP
-                entity_hp = pm.read_int(entity_pawn + m_iHealth)
-
-                # Выводим HP слева вверху от бокса
-                draw_list.add_text(leftX - 25, head_pos[1] - 5, color, str(entity_hp))
-
-            except:
-                continue
     except:
         return
+
+    # Перебираем возможные слоты сущностей
+    for i in range(64):
+        entity = pm.read_longlong(client + dwEntityList)
+        if not entity:
+            continue
+
+        # Находим адрес entity_controller
+        list_entry = pm.read_longlong(entity + ((8 * (i & 0x7FFF) >> 9) + 16))
+        if not list_entry:
+            continue
+
+        entity_controller = pm.read_longlong(list_entry + (120) * (i & 0x1FF))
+        if not entity_controller:
+            continue
+
+        # Получаем pawn из контроллера
+        entity_controller_pawn = pm.read_longlong(entity_controller + m_hPlayerPawn)
+        if not entity_controller_pawn:
+            continue
+
+        # Достаем сам pawn
+        list_entry = pm.read_longlong(entity + (0x8 * ((entity_controller_pawn & 0x7FFF) >> 9) + 16))
+        if not list_entry:
+            continue
+
+        entity_pawn = pm.read_longlong(list_entry + (120) * (entity_controller_pawn & 0x1FF))
+        if not entity_pawn or entity_pawn == local_player:
+            continue
+
+        # Проверяем жив ли (lifeState == 256)
+        if pm.read_int(entity_pawn + m_lifeState) != 256:
+            continue
+
+        # Пропускаем тиммейтов
+        if pm.read_int(entity_pawn + m_iTeamNum) == local_team:
+            continue
+
+        # Цвет бокса
+        color = imgui.get_color_u32_rgba(1, 0, 0, 1)
+
+        # Достаем указатель на матрицу костей
+        game_scene = pm.read_longlong(entity_pawn + m_pGameSceneNode)
+        bone_matrix = pm.read_longlong(game_scene + m_modelState + 0x80)
+
+        try:
+            # Координаты головы
+            headX = pm.read_float(bone_matrix + 6 * 0x20)
+            headY = pm.read_float(bone_matrix + 6 * 0x20 + 0x4)
+            headZ = pm.read_float(bone_matrix + 6 * 0x20 + 0x8) + 8
+            head_pos = w2s(view_matrix, headX, headY, headZ, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+            # Координаты ног
+            legZ = pm.read_float(bone_matrix + 28 * 0x20 + 0x8)
+            leg_pos = w2s(view_matrix, headX, headY, legZ, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+            # Рисуем бокс
+            delta = abs(head_pos[1] - leg_pos[1])
+            leftX = head_pos[0] - delta // 3
+            rightX = head_pos[0] + delta // 3
+
+            # Линии бокса
+            draw_list.add_line(leftX,  leg_pos[1],  rightX, leg_pos[1],  color, 2.0)
+            draw_list.add_line(leftX,  leg_pos[1],  leftX,  head_pos[1], color, 2.0)
+            draw_list.add_line(rightX, leg_pos[1],  rightX, head_pos[1], color, 2.0)
+            draw_list.add_line(leftX,  head_pos[1], rightX, head_pos[1], color, 2.0)
+
+            # Читаем HP
+            entity_hp = pm.read_int(entity_pawn + m_iHealth)
+
+            # Выводим HP слева вверху от бокса
+            # Слегка сместим на 25 пикселей влево и 5 пикселей вверх
+            draw_list.add_text(leftX - 25, head_pos[1] - 5, color, str(entity_hp))
+
+        except:
+            continue
 
 def main():
     # Инициализация glfw
