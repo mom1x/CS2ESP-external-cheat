80ad594a5b65e01272e98cb5bce833bac1ea8b28
Mom1x <momixpro8@gmail.com>
2026-05-19 18:50:02 +0700

    Update CS2ESP.py

--- CS2ESP.py
+++ CS2ESP.py
@@ -7,23 +7,43 @@ from imgui.integrations.glfw import GlfwRenderer
 import glfw
 import OpenGL.GL as gl
 import requests
+import ctypes
+import sys
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 
-offsets = requests.get('https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json').json()
-client_dll = requests.get('https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json').json()
+# Принудительная проверка прав администратора при запуске в коде
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
-dwEntityList = offsets['client.dll']['dwEntityList']
-dwLocalPlayerPawn = offsets['client.dll']['dwLocalPlayerPawn']
-dwViewMatrix = offsets['client.dll']['dwViewMatrix']
+if not is_admin():
+    # Запрашиваем повышение прав у Windows
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.exit()
 
-m_iTeamNum = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_iTeamNum']
-m_lifeState = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_lifeState']
-m_pGameSceneNode = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_pGameSceneNode']
-m_modelState = client_dll['client.dll']['classes']['CSkeletonInstance']['fields']['m_modelState']
-m_hPlayerPawn = client_dll['client.dll']['classes']['CCSPlayerController']['fields']['m_hPlayerPawn']
-m_iHealth = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_iHealth']
+# Безопасное получение оффсетов
+try:
+    offsets = requests.get('https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json').json()
+    client_dll = requests.get('https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json').json()
+
+    dwEntityList = offsets['client.dll']['dwEntityList']
+    dwLocalPlayerPawn = offsets['client.dll']['dwLocalPlayerPawn']
+    dwViewMatrix = offsets['client.dll']['dwViewMatrix']
+
+    m_iTeamNum = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_iTeamNum']
+    m_lifeState = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_lifeState']
+    m_pGameSceneNode = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_pGameSceneNode']
+    m_modelState = client_dll['client.dll']['classes']['CSkeletonInstance']['fields']['m_modelState']
+    m_hPlayerPawn = client_dll['client.dll']['classes']['CCSPlayerController']['fields']['m_hPlayerPawn']
+    m_iHealth = client_dll['client.dll']['classes']['C_BaseEntity']['fields']['m_iHealth']
+except Exception as e:
+    print(f"Ошибка загрузки оффсетов: {e}")
+    time.sleep(5)
+    sys.exit()
 
 # Ожидаем запуска cs2.exe
 while True:
@@ -35,113 +55,102 @@ while True:
     except:
         pass
 
-time.sleep(1)
-os.system("cls")
-
-pm = pymem.Pymem("cs2.exe")
-client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
-
 def w2s(mtx, posx, posy, posz, width, height):
-    screenW = mtx[12]*posx + mtx[13]*posy + mtx[14]*posz + mtx[15]
-    if screenW > 0.001:
-        screenX = mtx[0]*posx + mtx[1]*posy + mtx[2]*posz + mtx[3]
-        screenY = mtx[4]*posx + mtx[5]*posy + mtx[6]*posz + mtx[7]
-        camX = width / 2
-        camY = height / 2
-        x = camX + (camX * screenX / screenW) // 1
-        y = camY - (camY * screenY / screenW) // 1
-        return [x, y]
+    try:
+        screenW = mtx[12]*posx + mtx[13]*posy + mtx[14]*posz + mtx[15]
+        if screenW > 0.001:
+            screenX = mtx[0]*posx + mtx[1]*posy + mtx[2]*posz + mtx[3]
+            screenY = mtx[4]*posx + mtx[5]*posy + mtx[6]*posz + mtx[7]
+            camX = width / 2
+            camY = height / 2
+            x = camX + (camX * screenX / screenW) // 1
+            y = camY - (camY * screenY / screenW) // 1
+            return [x, y]
+    except:
+        pass
     return [-999, -999]
 
 def esp(draw_list):
-    # Считываем view matrix
-    view_matrix = [pm.read_float(client + dwViewMatrix + i * 4) for i in range(16)]
-
-    # Получаем локального игрока и его команду
-    local_player = pm.read_longlong(client + dwLocalPlayerPawn)
+    # Защищаем базовое чтение локального игрока
     try:
+        view_matrix = [pm.read_float(client + dwViewMatrix + i * 4) for i in range(16)]
+        local_player = pm.read_longlong(client + dwLocalPlayerPawn)
+        if not local_player:
+            return
         local_team = pm.read_int(local_player + m_iTeamNum)
     except:
         return
 
-    # Перебираем возможные слоты сущностей
+    # Защищаем весь цикл перебора сущностей — теперь падений не будет!
     for i in range(64):
-        entity = pm.read_longlong(client + dwEntityList)
-        if not entity:
-            continue
-
-        # Находим адрес entity_controller
-        list_entry = pm.read_longlong(entity + ((8 * (i & 0x7FFF) >> 9) + 16))
-        if not list_entry:
-            continue
-
-        entity_controller = pm.read_longlong(list_entry + (120) * (i & 0x1FF))
-        if not entity_controller:
-            continue
-
-        # Получаем pawn из контроллера
-        entity_controller_pawn = pm.read_longlong(entity_controller + m_hPlayerPawn)
-        if not entity_controller_pawn:
-            continue
-
-        # Достаем сам pawn
-        list_entry = pm.read_longlong(entity + (0x8 * ((entity_controller_pawn & 0x7FFF) >> 9) + 16))
-        if not list_entry:
-            continue
-
-        entity_pawn = pm.read_longlong(list_entry + (120) * (entity_controller_pawn & 0x1FF))
-        if not entity_pawn or entity_pawn == local_player:
-            continue
-
-        # Проверяем жив ли (lifeState == 256)
-        if pm.read_int(entity_pawn + m_lifeState) != 256:
-            continue
-
-        # Пропускаем тиммейтов
-        if pm.read_int(entity_pawn + m_iTeamNum) == local_team:
-            continue
-
-        # Цвет бокса
-        color = imgui.get_color_u32_rgba(1, 0, 0, 1)
-
-        # Достаем указатель на матрицу костей
-        game_scene = pm.read_longlong(entity_pawn + m_pGameSceneNode)
-        bone_matrix = pm.read_longlong(game_scene + m_modelState + 0x80)
-
         try:
-            # Координаты головы
+            entity = pm.read_longlong(client + dwEntityList)
+            if not entity:
+                continue
+
+            list_entry = pm.read_longlong(entity + ((8 * (i & 0x7FFF) >> 9) + 16))
+            if not list_entry:
+                continue
+
+            entity_controller = pm.read_longlong(list_entry + (120) * (i & 0x1FF))
+            if not entity_controller:
+                continue
+
+            entity_controller_pawn = pm.read_longlong(entity_controller + m_hPlayerPawn)
+            if not entity_controller_pawn:
+                continue
+
+            list_entry = pm.read_longlong(entity + (0x8 * ((entity_controller_pawn & 0x7FFF) >> 9) + 16))
+            if not list_entry:
+                continue
+
+            entity_pawn = pm.read_longlong(list_entry + (120) * (entity_controller_pawn & 0x1FF))
+            if not entity_pawn or entity_pawn == local_player:
+                continue
+
+            if pm.read_int(entity_pawn + m_lifeState) != 256:
+                continue
+
+            if pm.read_int(entity_pawn + m_iTeamNum) == local_team:
+                continue
+
+            color = imgui.get_color_u32_rgba(1, 0, 0, 1)
+
+            game_scene = pm.read_longlong(entity_pawn + m_pGameSceneNode)
+            if not game_scene:
+                continue
+                
+            bone_matrix = pm.read_longlong(game_scene + m_modelState + 0x80)
+            if not bone_matrix:
+                continue
+
             headX = pm.read_float(bone_matrix + 6 * 0x20)
             headY = pm.read_float(bone_matrix + 6 * 0x20 + 0x4)
             headZ = pm.read_float(bone_matrix + 6 * 0x20 + 0x8) + 8
             head_pos = w2s(view_matrix, headX, headY, headZ, WINDOW_WIDTH, WINDOW_HEIGHT)
 
-            # Координаты ног
             legZ = pm.read_float(bone_matrix + 28 * 0x20 + 0x8)
             leg_pos = w2s(view_matrix, headX, headY, legZ, WINDOW_WIDTH, WINDOW_HEIGHT)
 
-            # Рисуем бокс
+            if head_pos[0] == -999 or leg_pos[0] == -999:
+                continue
+
             delta = abs(head_pos[1] - leg_pos[1])
             leftX = head_pos[0] - delta // 3
             rightX = head_pos[0] + delta // 3
 
-            # Линии бокса
-            draw_list.add_line(leftX,  leg_pos[1],  rightX, leg_pos[1],  color, 2.0)
-            draw_list.add_line(leftX,  leg_pos[1],  leftX,  head_pos[1], color, 2.0)
-            draw_list.add_line(rightX, leg_pos[1],  rightX, head_pos[1], color, 2.0)
-            draw_list.add_line(leftX,  head_pos[1], rightX, head_pos[1], color, 2.0)
+            draw_list.add_line(leftX, leg_pos[1], rightX, leg_pos[1], color, 2.0)
+            draw_list.add_line(leftX, leg_pos[1], leftX, head_pos[1], color, 2.0)
+            draw_list.add_line(rightX, leg_pos[1], rightX, head_pos[1], color, 2.0)
+            draw_list.add_line(leftX, head_pos[1], rightX, head_pos[1], color, 2.0)
 
-            # Читаем HP
             entity_hp = pm.read_int(entity_pawn + m_iHealth)
-
-            # Выводим HP слева вверху от бокса
-            # Слегка сместим на 25 пикселей влево и 5 пикселей вверх
             draw_list.add_text(leftX - 25, head_pos[1] - 5, color, str(entity_hp))
-
         except:
+            # Если адрес памяти недоступен — просто пропускаем итерацию, не ломая оверлей
             continue
 
 def main():
-    # Инициализация glfw
     glfw.init()
     glfw.window_hint(glfw.TRANSPARENT_FRAMEBUFFER, glfw.TRUE)
     window = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "overlay", None, None)
@@ -159,7 +168,6 @@ def main():
     imgui.create_context()
     impl = GlfwRenderer(window)
 
-    # Основной цикл
     while not glfw.window_should_close(window):
         glfw.poll_events()
         impl.process_inputs()
@@ -175,7 +183,12 @@ def main():
                           imgui.WINDOW_NO_BACKGROUND)
 
         draw_list = imgui.get_window_draw_list()
-        esp(draw_list)
+        
+        # Безопасный вызов отрисовки
+        try:
+            esp(draw_list)
+        except:
+            pass
 
         imgui.end()
         imgui.end_frame()
